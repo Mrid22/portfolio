86064a62cdc68b9300580e3194cd2a5f90e6b7df
Mrid22 <mridulaga@outlook.com>
2025-04-18 01:59:02 +0800

    testing react router dom

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+export default function App() {
   return (
-    <main>
-      <div>Next.js on GitHub Pages</div>
-    </main>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
