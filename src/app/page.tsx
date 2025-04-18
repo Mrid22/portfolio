b5c57c9749d1e993c87139ccefd7ae19d1d1cdc5
Mrid22 <mridulaga@outlook.com>
2025-04-18 17:45:40 +0800

    fixed image positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,7 @@ export default function App() {
   return (
     <>
       <section id="hero" className="relative top-0 left-0 w-full h-full">
-        <Image
-          src={Hero}
-          alt="Hero Image"
-          fill
-          className="relative top-0 left-0"
-        />
+        <Image src={Hero} alt="Hero Image" fill className="top-0 left-0" />
       </section>
       <section id="projects" className="grid w-screen h-screen grid-cols-2">
         <ProjectCard />
