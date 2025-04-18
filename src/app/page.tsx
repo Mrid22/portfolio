2f1647ef302476f6f8ef065c6b1b523f67bfaf11
Mrid22 <mridulaga@outlook.com>
2025-04-18 17:16:21 +0800

    made hero section relative so flexbox wors correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Hero from "@/src/assets/hero.jpg";
 export default function App() {
   return (
     <>
-      <section id="hero" className="absolute top-0 left-0 w-full h-full">
+      <section id="hero" className="relative top-0 left-0 w-full h-full">
         <Image src={Hero} alt="Hero Image" fill className="object-cover" />
       </section>
       <section id="projects" className="grid grid-cols-2">
