3c574b937d1e2dddbdf870ae98e00d43d21bb06b
Mrid22 <mridulaga@outlook.com>
2025-04-18 16:07:48 +0800

    added link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ export default function Footer() {
   return (
     <div className="w-full z-50">
       <p className="text-white">hello</p>
+      <a href="www.github.com/Mrid22"></a>
     </div>
   );
 }
