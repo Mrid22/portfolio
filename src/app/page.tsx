f0c0429f50a24c25c8993ffe8dbc874b2c378df2
Mrid22 <mridulaga@outlook.com>
2025-04-18 17:30:12 +0800

    trying to get hero to respect flexbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import Hero from "@/src/assets/hero.jpg";
 export default function App() {
   return (
     <>
-      <section id="hero" className="relative top-0 left-0 w-full h-full">
+      <section id="hero" className="fixed top-0 left-0 w-full h-full">
         <Image src={Hero} alt="Hero Image" fill className="object-cover" />
       </section>
-      <section id="projects" className="grid grid-cols-2">
+      <section id="projects" className="grid w-screen h-screen grid-cols-2">
         <ProjectCard />
         <ProjectCard />
       </section>
