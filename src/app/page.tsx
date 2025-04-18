633ff28780be19ce78d31001ffab8fe0b5c276d1
Mrid22 <mridulaga@outlook.com>
2025-04-18 18:18:32 +0800

    added cards for all categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,10 @@ export default function App() {
       <section id="hero" className="relative top-0 left-0 w-screen h-screen">
         <Image src={Hero} alt="Hero Image" fill className="top-0 left-0" />
       </section>
-      <section id="projects" className="grid w-screen h-screen grid-cols-2">
+      <section
+        id="projects"
+        className="grid w-screen h-screen grid-cols-2 grid-rows-2"
+      >
         <ProjectCard
           title="UI Design"
           description="My UI Design projects"
@@ -21,6 +24,18 @@ export default function App() {
           description="My Python projects"
           img_src={Hero}
           img_desc="picture for Python"
+        />
+        <ProjectCard
+          title="Web Development"
+          description="My Web Development projects"
+          img_src={Hero}
+          img_desc="picture for Web Development"
+        />
+        <ProjectCard
+          title="Nix OS"
+          description="My Nix OS configuration"
+          img_src={Hero}
+          img_desc="picture for Nix  OS"
         />
       </section>
     </>
