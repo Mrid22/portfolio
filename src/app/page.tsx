5c5a51f5b234571f9158623c10e791a475dbaaf3
Mrid22 <mridulaga@outlook.com>
2025-04-19 03:40:11 +0800

    redoing main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,41 +6,15 @@ import Hero from "@/src/assets/hero.jpg";
 export default function App() {
   return (
     <>
-      <section id="hero" className="relative top-0 left-0 w-screen h-screen">
-        <Image src={Hero} alt="Hero Image" fill className="top-0 left-0" />
-      </section>
-      <section
-        id="projects"
-        className="grid w-screen h-screen grid-cols-2 grid-rows-2 gap-5"
-      >
+      <section></section>
+      <section>
         <ProjectCard
           title="UI Design"
-          description="My UI Design projects"
           img_src={Hero}
-          img_desc="picture for Ui design"
+          img_desc="UI Design"
+          description="My UI Design Projects"
           link="/projects/ui-design"
-        />
-        <ProjectCard
-          title="Python"
-          description="My Python projects"
-          img_src={Hero}
-          img_desc="picture for Python"
-          link="/projects/python"
-        />
-        <ProjectCard
-          title="Web Development"
-          description="My Web Development projects"
-          img_src={Hero}
-          img_desc="picture for Web Development"
-          link="/projects/web-development"
-        />
-        <ProjectCard
-          title="Nix OS"
-          description="My Nix OS configuration"
-          img_src={Hero}
-          img_desc="picture for Nix  OS"
-          link="/projects/nixos"
-        />
+        ></ProjectCard>
       </section>
     </>
   );
