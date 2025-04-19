ffccef418a8419aa7f4bfc345e8ef99877b1cabf
Mrid22 <mridulaga@outlook.com>
2025-04-19 14:34:48 +0800

    added grid layout and card for web dev to test it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,23 @@ export default function App() {
       <section id="hero" className="w-screen h-screen">
         <Image src={Hero} alt="Hero Image" fill></Image>
       </section>
-      <section id="projects" className="w-screen h-screen box-border p-2">
+      <section
+        id="projects"
+        className="w-screen h-screen box-border p-2 grid grid-cols-2"
+      >
         <ProjectCard
-          img_src={Hero}
-          img_desc="UI design img"
           title="UI Design"
           description="My UI Design Projects"
+          img_src={Hero}
+          img_desc="UI design img"
           link="/projects/ui-design"
+        ></ProjectCard>
+        <ProjectCard
+          title="Web Development"
+          description="My Web Development Projects"
+          img_src={Hero}
+          img_desc="Web Dev img"
+          link="/projects/web-development"
         ></ProjectCard>
       </section>
     </>
