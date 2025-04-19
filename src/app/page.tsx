8b2a8a12187298cd7c0f0df8d0d61fff6f2154ac
Mrid22 <mridulaga@outlook.com>
2025-04-19 14:00:27 +0800

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,23 @@ import Hero from "@/src/assets/hero.jpg";
 export default function App() {
   return (
     <>
-      <section></section>
-      <section id="projects" className="grid grid-cols-2 p-0">
+      <section id="hero" className="w-screen h-screen">
+        <Image src={Hero} alt="Hero Image" fill></Image>
+      </section>
+      <section id="projects" className="grid grid-cols-2 p-0 w-screen h-screen">
         <ProjectCard
           title="UI Design"
           img_src={Hero}
           img_desc="UI Design"
           description="My UI Design Projects"
           link="/projects/ui-design"
+        ></ProjectCard>
+        <ProjectCard
+          title="Web Development"
+          img_src={Hero}
+          img_desc="Python Image"
+          description="My Python Projects"
+          link="/projects/python"
         ></ProjectCard>
       </section>
     </>
