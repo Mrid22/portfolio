6a3d958d0e09b8b9e4bc0562f232c998521efbd1
Mrid22 <mridulaga@outlook.com>
2025-04-19 15:48:16 +0800

    trying to fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="relative w-screen z-50 h-10 p-0">
       <p className="text-white">hello</p>
-      <a href="www.github.com/Mrid22"></a>
+      <a href="www.github.com/Mrid22">held</a>
     </div>
   );
 }
