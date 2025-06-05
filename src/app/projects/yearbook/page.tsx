bfe42839d534fcbc40b709b0494155116e86ca2a
Mrid22 <mridulaga@outlook.com>
2025-06-05 19:47:58 +0800

    trying out test collabsible

--- src/app/projects/yearbook/page.tsx
+++ src/app/projects/yearbook/page.tsx
@@ -4,24 +4,11 @@ import {
   CollapsibleContent,
 } from "@/src/components/ui/collapsible";
 export default function Yearbook() {
-  <div className="flex flex-col">
-    <Collapsible>
-      <CollapsibleTrigger>Layout Member (2022-2025)</CollapsibleTrigger>
-      <CollapsibleContent>
-        <p>sadf</p>
-      </CollapsibleContent>
-    </Collapsible>
-    <Collapsible>
-      <CollapsibleTrigger>Treasurer (2023-2024)</CollapsibleTrigger>
-      <CollapsibleContent>
-        <p>Treasurer (2023-2024)</p>
-      </CollapsibleContent>
-    </Collapsible>
-    <Collapsible>
-      <CollapsibleTrigger>Layout Editor (2024-2025)</CollapsibleTrigger>
-      <CollapsibleContent>
-        <p>Treasurer (2023-2023)</p>
-      </CollapsibleContent>
-    </Collapsible>
-  </div>;
+  <Collapsible>
+    <CollapsibleTrigger>Can I use this in my project?</CollapsibleTrigger>
+    <CollapsibleContent>
+      Yes. Free to use for personal and commercial projects. No attribution
+      required.
+    </CollapsibleContent>
+  </Collapsible>;
 }
