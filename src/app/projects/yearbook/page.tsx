aaf41430668a76feb0d9647792cf0e435424999f
Mrid22 <mridulaga@outlook.com>
2025-06-05 19:32:20 +0800

    aded mock content to yearbook page

--- src/app/projects/yearbook/page.tsx
+++ src/app/projects/yearbook/page.tsx
@@ -5,15 +5,21 @@ import {
 } from "@radix-ui/react-collapsible";
 export default function Yearbook() {
   <Collapsible>
-    <CollapsibleTrigger>Treasurer (2023-2023)</CollapsibleTrigger>
-    <CollapsibleContent></CollapsibleContent>
+    <CollapsibleTrigger>Layout Member (2022-2025)</CollapsibleTrigger>
+    <CollapsibleContent>
+      <p>sadf</p>
+    </CollapsibleContent>
+  </Collapsible>;
+  <Collapsible>
+    <CollapsibleTrigger>Treasurer (2023-2024)</CollapsibleTrigger>
+    <CollapsibleContent>
+      <p>Treasurer (2023-2024)</p>
+    </CollapsibleContent>
   </Collapsible>;
   <Collapsible>
     <CollapsibleTrigger>Layout Editor (2024-2025)</CollapsibleTrigger>
-    <CollapsibleContent></CollapsibleContent>
-  </Collapsible>;
-  <Collapsible>
-    <CollapsibleTrigger>Layout Member (2022-2025)</CollapsibleTrigger>
-    <CollapsibleContent></CollapsibleContent>
+    <CollapsibleContent>
+      <p>Treasurer (2023-2023)</p>
+    </CollapsibleContent>
   </Collapsible>;
 }
