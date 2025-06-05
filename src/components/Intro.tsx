e909b0ea5e24999fe6e0307ca83b592fc91c3505
Mrid22 <mridulaga@outlook.com>
2025-06-05 21:09:40 +0800

    added intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,1 +1,15 @@
-import { Dialog } from "./ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogTitle,
+  DialogDescription,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "./ui/dialog";
+import * as React from "react";
+export default function Intro() {
+  const [open, setOpen] = React.useState(false);
+  return <Dialog open={open} onOpenChange={setOpen} modal={true}></Dialog>;
+}
