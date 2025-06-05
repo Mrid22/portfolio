7b0cf1220343690232bf7b1cfec5c44402782112
Mrid22 <mridulaga@outlook.com>
2025-06-05 22:31:51 +0800

    made figma take up more screen space

--- src/app/projects/ui-design/page.tsx
+++ src/app/projects/ui-design/page.tsx
@@ -2,15 +2,14 @@ export default function UIDesign() {
   return (
     <div className="flex flex-col gap-1">
       <iframe
-        width="800"
-        height="450"
+        width="1000"
+        height="1000"
         src="https://embed.figma.com/proto/IgLxlXTgFN88hrXOBfhTF6/Portfolio-Wireframes?node-id=257-1504&amp;p=f&amp;scaling=scale-down&amp;content-scaling=fixed&amp;page-id=257%3A1119&amp;starting-point-node-id=257%3A1504&amp;embed-host=share"
         allowFullScreen
       ></iframe>
       <iframe
-        className="w-full h-full"
-        width="100%"
-        height="100%"
+        width="1000"
+        height="1000"
         src="https://embed.figma.com/design/xhmMtlwtoQD14kJ6hJgh5X/Portfolio-Website?node-id=192-677&amp;embed-host=share"
         allowFullScreen
       ></iframe>
