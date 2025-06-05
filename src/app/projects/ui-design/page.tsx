fa04d7255ff31a2bfe2e837ec64069734f29fbe1
Mrid22 <mridulaga@outlook.com>
2025-06-05 22:33:42 +0800

    again

--- src/app/projects/ui-design/page.tsx
+++ src/app/projects/ui-design/page.tsx
@@ -2,7 +2,7 @@ export default function UIDesign() {
   return (
     <div className="flex flex-col gap-1">
       <iframe
-        width="1000"
+        width="100%"
         height="1000"
         src="https://embed.figma.com/proto/IgLxlXTgFN88hrXOBfhTF6/Portfolio-Wireframes?node-id=257-1504&amp;p=f&amp;scaling=scale-down&amp;content-scaling=fixed&amp;page-id=257%3A1119&amp;starting-point-node-id=257%3A1504&amp;embed-host=share"
         allowFullScreen
