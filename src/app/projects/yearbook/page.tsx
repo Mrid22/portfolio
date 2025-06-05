c2d4055b2f501e64d6c92b9bba5d42af5f1b5284
Mrid22 <mridulaga@outlook.com>
2025-06-05 19:34:24 +0800

    trying to fix yearbook page

--- src/app/projects/yearbook/page.tsx
+++ src/app/projects/yearbook/page.tsx
@@ -4,22 +4,27 @@ import {
   CollapsibleTrigger,
 } from "@radix-ui/react-collapsible";
 export default function Yearbook() {
-  <Collapsible>
-    <CollapsibleTrigger>Layout Member (2022-2025)</CollapsibleTrigger>
-    <CollapsibleContent>
-      <p>sadf</p>
-    </CollapsibleContent>
-  </Collapsible>;
-  <Collapsible>
-    <CollapsibleTrigger>Treasurer (2023-2024)</CollapsibleTrigger>
-    <CollapsibleContent>
-      <p>Treasurer (2023-2024)</p>
-    </CollapsibleContent>
-  </Collapsible>;
-  <Collapsible>
-    <CollapsibleTrigger>Layout Editor (2024-2025)</CollapsibleTrigger>
-    <CollapsibleContent>
-      <p>Treasurer (2023-2023)</p>
-    </CollapsibleContent>
-  </Collapsible>;
+  <div className="flex flex-col">
+    <Collapsible>
+      <CollapsibleTrigger>Layout Member (2022-2025)</CollapsibleTrigger>
+      <CollapsibleContent>
+        <p>sadf</p>
+      </CollapsibleContent>
+    </Collapsible>
+    ;
+    <Collapsible>
+      <CollapsibleTrigger>Treasurer (2023-2024)</CollapsibleTrigger>
+      <CollapsibleContent>
+        <p>Treasurer (2023-2024)</p>
+      </CollapsibleContent>
+    </Collapsible>
+    ;
+    <Collapsible>
+      <CollapsibleTrigger>Layout Editor (2024-2025)</CollapsibleTrigger>
+      <CollapsibleContent>
+        <p>Treasurer (2023-2023)</p>
+      </CollapsibleContent>
+    </Collapsible>
+    ;
+  </div>;
 }
